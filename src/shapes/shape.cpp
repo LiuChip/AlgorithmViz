b5de607566dd33bd707ed0230788ab41c30351e4
刘驰 <87454242+LiuChip@users.noreply.github.com>
2026-07-10 00:15:48 +0800

    Updating Shape.

---
+++ src/shapes/shape.cpp
@@ -0,0 +1,3 @@
+#include "shape.h"
+int Shape::IdVal = 0;
+int Shape::Layer = 0;
--- src/shapes/shape.h
+++ src/shapes/shape.h
@@ -3,43 +3,102 @@
 #include <QString>
 #include <QVector>
 #include <QPointF>
+#include <QSizeF>
+#include <QFont>
+#include <QGraphicsItem>
 
-class Shape
+class Shape:public QGraphicsItem
 {
 protected:
+    //Static
+    static int IdVal;
+    static int Layer;
+
     //Positon and Size
     qreal x,y;
-    qreal width,length;
+    qreal width,height;
     qreal rotation = 0.0;
 
     //Border Style
-    qreal borderWidth = 0.0;
-    QColor borderColor = 0.0;
-    Qt::PenStyle borderStyle;
+    struct BorderStyle{
+        qreal borderWidth = 0.0;
+        QColor borderColor = Qt::transparent;
+        Qt::PenStyle borderStyle = Qt::NoPen;
+    };
 
     //Filling Style
-    QColor fillColor = Qt::transparent;
-    qreal fillOpacity = 1.0; //transparency
-
+    struct FillStyle{
+        QColor fillColor = Qt::transparent;
+        qreal fillOpacity = 1.0; //transparency
+    };
     //Text Style
-    QString text = "";
-    qreal fontSize = 16;
-    QString fontFamily = "Arial";
-    int fontWeight;
-    QColor textColor = Qt::black;
-    Qt::AlignmentFlag textAligh = Qt::AlignCenter;
+    struct TextStyle{
+        QString text = "";
+        qreal fontSize = 16;
+        QString fontFamily = "Arial";
+        int fontWeight = QFont::Normal;
+        QColor textColor = Qt::black;
+        Qt::AlignmentFlag textAligh = Qt::AlignCenter;
+    };
 
     //Indentify
     int id = -1;
     QString name = "";
     bool visible = true;
-    int layer;
+    int layer = 0;
     bool lock_stat = false;
 
     //Link Stat
     QVector<QPointF> anchorPoints;
 
+private:
+    void setID();
 public:
-    QPointF getPosition();
+
+    //Getter & Setter
+    QPointF getPosition() const;
+    void setPosition(QPointF point);
+    void setPosition(qreal x,qreal y);
+    QSizeF getSize() const;
+    void setSize(QSizeF size);
+    void setSize(qreal width,qreal height);
+    qreal getRotation() const;
+    void setRotation(qreal rotation);
+
+    BorderStyle getBorderInfo() const;
+    void setBorderInfo(BorderStyle borderstyle);
+    void setBorderInfo();
+
+    FillStyle getFillInfo() const;
+    void setFillInfo();
+    void setFillInfo(FillStyle fillstyle);
+
+    TextStyle getTextInfo() const;
+    void setTextInfo();
+    void setTextInfo(TextStyle textstyle);
+
+    int getID() const;
+
+    QString getName() const;
+    void setName(QString name = "");
+
+    bool getVisible() const;
+    void changeVisibility();
+
+    int getLayer() const;
+    void setLayer(bool upper = true); //Shift Layer
+    void setLayer(int layer);
+
+    bool getLockStat() const;
+    void changeLockStat();
+
+    //Function
+    QRectF boundingRect() const override = 0;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override = 0;
+
+    //Constructor
+    Shape();
+    Shape(qreal x, qreal y, qreal width, qreal height);
+    Shape(QPointF point,QSizeF size);
 
 };
